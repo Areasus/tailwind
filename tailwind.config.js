49a5189c3a05442f04f20c3150926932d2389bfd
Areasus <cantosh.s@gmail.com>
2022-06-17 14:05:56 +0545

    added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontSize:{
+        'custom':['8rem',{lineHeight:'1'}]
+      }
+    },
+  },
+  plugins: [],
+}
